ad5b0e2e2d6fd8161e1477b702485f8f531621c9
Ryan Goh <ryangohruien@gmail.com>
2023-09-01 16:22:56 +1000

    added python import datafile

---
+++ importData.py
@@ -0,0 +1,33 @@
+import mysql.connector
+
+# Replace these values with your MySQL server details
+host = "localhost"
+user = "root"
+password = "-" #INSERT password here
+database = "mydb"
+
+# Create a connection
+connection = mysql.connector.connect(
+    host=host,
+    user=user,
+    password=password,
+    database=database
+)
+
+cursor = connection.cursor()
+
+table_name = "users"
+data_to_upload = [
+    ("2","Johny", "Doep", "johnydoep@example.com")
+    # Add more data rows as needed
+]
+
+insert_query = f"INSERT INTO {table_name} VALUES (%s, %s, %s, %s)"
+
+cursor.executemany(insert_query, data_to_upload)
+
+# Commit the changes to the database
+connection.commit()
+
+cursor.close()
+connection.close()
