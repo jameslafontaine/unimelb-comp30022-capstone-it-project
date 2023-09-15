e80c4e7ce23a09e2a9592a818b1f59ea6207f051
lettussandwich <127374405+lettussandwich@users.noreply.github.com>
2023-09-15 20:24:32 +1000

    begun JS work

--- instructorView/static/instructorView/test.js
+++ instructorView/static/instructorView/test.js
@@ -1,9 +1,10 @@
+
 hello = function() {
     fetch('/get_data/')
         .then(response => response.json())
         .then(data => {
             // Update the page with the retrieved data
-            document.getElementById('headerText').innerHTML = "Signed in as: " + JSON.stringify(data);
+            document.getElementById('headerText').innerHTML = "Signed in as: " + data.ID;
         })
         .catch(error => {
             console.error('Error:', error);
