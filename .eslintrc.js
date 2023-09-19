a0b3328f961e540830520d23182667ff090ac6d2
Jun Youn <juuyoun01@gmail.com>
2023-09-19 10:12:56 +1000

    further eslint configurations

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  // Specify the environments where your code will run
+  env: {
+    browser: true, // For browser-based projects
+    node: true,    // For Node.js projects
+    es6: true,     // For ES6 features
+  },
+  // Define the set of rules you want to enforce
+  extends: 'eslint:recommended', // You can extend other ESLint configurations here
+
+  // Add any custom rules or overrides
+  rules: {
+    // Example custom rule: disallow the use of console.log()
+    'no-console': 'error',
+    
+    // Add more rules or customize as needed
+  },
+};
