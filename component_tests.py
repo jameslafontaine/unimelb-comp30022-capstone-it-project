3222643c978f87a69a0f86861cdaa17d4de8d1c4
Jun Youn <juuyoun01@gmail.com>
2023-10-19 23:24:26 +1100

    added skeleton login component tests

--- component_tests.py
+++ component_tests.py
@@ -5,6 +5,26 @@ Component tests
 
 import requests
 
+def test_login_endpoint():
+    '''
+    Login endpoint is working
+    Acceptance criteria:
+        - Returns 200
+    '''
+    response = requests.get('http://localhost:8000/', timeout = 5)
+    assert response.status_code == 200, "login page returns 404"
+
+def test_login_as_instructor():
+    '''
+    Test login as instructor
+    Acceptance criteria:
+        - Generate OAuth Authorization token
+        - Instructor view returns 200
+    '''
+    # Generate OAuth token and try instructor home page
+    assert True, "OAuth has not been implemented yet"
+
+
 def test_instructor_view_nonvariable_endpoints():
     '''
     In Instructor View, testing for:
@@ -14,11 +34,22 @@ def test_instructor_view_nonvariable_endpoints():
     Acceptance criteria:
         - Returns 200
     '''
+    # Implement OAuth when Canvas is integrated
     instructor_view_nonvariable_endpoints = ['']
     for endpoint in instructor_view_nonvariable_endpoints:
         response = requests.get('http://localhost:8000/instructor/' + endpoint, timeout = 5)
         assert response.status_code == 200, \
             "/instructor/" + endpoint + " has error"
+
+def test_login_as_student():
+    '''
+    Test login as student
+    Acceptance criteria:
+        - Generate OAuth Authorization token
+        - Student view returns 200
+    '''
+    # Generate OAuth token and try student home page
+    assert True, "OAuth has not been implemented yet"
 
 def test_student_view_nonvariable_endpoints():
     '''
@@ -29,6 +60,7 @@ def test_student_view_nonvariable_endpoints():
     Acceptance criteria:
         - Returns 200
     '''
+    # Implement OAuth when Canvas is integrated
     student_view_nonvariable_endpoints = ['']
     for endpoint in student_view_nonvariable_endpoints:
         response = requests.get('http://localhost:8000/student/' + endpoint, timeout = 5)
