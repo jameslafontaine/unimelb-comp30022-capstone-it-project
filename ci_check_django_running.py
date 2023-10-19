d7ba808a3b0ddf5e1e2df3f501c83ae99d28c12c
Jun Youn <juuyoun01@gmail.com>
2023-10-19 22:38:36 +1100

    add blackbox tests

--- ci_check_django_running.py
+++ ci_check_django_running.py
@@ -9,7 +9,7 @@ def ci_check_django_running():
     '''Ping localhost:8000 every 5 seconds'''
     while True:
         try:
-            response = requests.get("https://localhost:8000", timeout = 5)
+            response = requests.get("http://localhost:8000", timeout = 5)
             if response.status_code == 200:
                 return True
             time.sleep(5)
