fa546c1922ca15dddd2efba52561429eae0d2555
Jun Youn <juuyoun01@gmail.com>
2023-10-20 15:50:17 +1100

    filled out instructor_view UTs to current knowledge of codebase

--- instructor_view/tests.py
+++ instructor_view/tests.py
@@ -3,7 +3,10 @@ Request and Query Management System - Testing Suite
 instructor_view Unit Tests
 """
 
+import json
 import requests
+
+LOCALHOST_PATH = 'http://localhost:8000'
 
 def test_instructor_view_get_courses():
     '''
@@ -12,69 +15,80 @@ def test_instructor_view_get_courses():
     Acceptance criteria:
         - Returns all courses of user
     '''
-    response = requests.get('http://localhost:8000/instructor/courses/1', timeout = 5)
-    assert response.json() == {"courses": [course1, course1]}
+    response = requests.get(LOCALHOST_PATH + '/instructor/courses/1', timeout = 5)
+    assert response.json() == {"courses": json.dumps([course1, course1])}, \
+        "/instructor/courses/ endpoint failed"
 
 def test_instructor_view_get_request_status():
     '''
     Testing:
         - /instructor/request-status/<request_id>
     Acceptance criteria:
-        -
+        - Get request status of request_id
     '''
-    assert True, ""
+    response = requests.get(LOCALHOST_PATH +'/instructor/request-status/1', timeout = 5)
+    assert response.json() == {'status': 'balls'}, "/instructor/request-status/ endpoint failed"
 
 def test_instructor_view_get_request():
     '''
     Testing:
         - /instructor/request/<request_id>
     Acceptance criteria:
-        -
+        - Returns correct request
     '''
-    assert True, ""
+    response = requests.get(LOCALHOST_PATH +'/instructor/request/1', timeout = 5)
+    assert response.json() == req1, "/instructor/request/ endpoint failed"
 
 def test_instructor_view_get_request_old_versions():
     '''
     Testing:
         - /instructor/old-versions/<request_id>
     Acceptance criteria:
-        -
+        - Returns old versions of a request
     '''
-    assert True, ""
+    response = requests.get(LOCALHOST_PATH + '/instructor/old-versions/1', timeout = 5)
+    assert response.json() == {'request_ids':json.dumps([1,2,3])}, \
+        "/instructor/old-versions/ endpoint failed"
 
 def test_instructor_view_get_subject_settings():
     '''
     Testing:
         - /instructor/get-subject-settings/<subject_id>
     Acceptance criteria:
-        -
+        - Returns subject settings for a subject
     '''
-    assert True, ""
+    response = requests.get(LOCALHOST_PATH +'/instructor/get-subject-settings/1', timeout = 5)
+    assert response.json() == subj_settings, "/instructor/get-subject-settings/ endpoint failed"
 
 def test_instructor_view_get_aap():
     '''
     Testing:
         - /instructor/aap/<aap_id>
     Acceptance criteria:
-        -
+        - Gets AAP of aap_id
     '''
-    assert True, ""
+    response = requests.get(LOCALHOST_PATH + '/instructor/aap/1', timeout = 5)
+    assert response.json() == {'this is an aap': 'aap go beep boop'}, \
+        "/instructor/aap/ endpoint failed"
 
 def test_instructor_view_get_request_history():
     '''
     Testing:
         - /instructor/request-history/<student_id>
     Acceptance criteria:
-        -
+        - Gets request history of student
     '''
-    assert True, ""
+    response = requests.get(LOCALHOST_PATH + '/instructor/request-history/1', timeout = 5)
+    assert response.json() == { 'requests': json.dumps([req1, req2])}, \
+        "/instructor/request-history/ endpoint failed"
 
 def test_instructor_view_post_aap():
     '''
     Testing:
         - /instructor/aap/
     Acceptance criteria:
-        -
+        - Post an AAP
+        - Call DB
     '''
     assert True, ""
 
@@ -83,27 +97,36 @@ def test_instructor_view_set_complex():
     Testing:
         - /instructor/set-complex/<request_id>
     Acceptance criteria:
-        -
+        - Case has been made complex
+        - Call DB
     '''
-    assert True, ""
+    response = requests.put(LOCALHOST_PATH + '/instructor/set-complex/1', timeout = 5)
+    assert response.json() == {"message": "Made complex"}, \
+        "/instructor/set-complex endpoint failed"
 
 def test_instructor_view_request_response():
     '''
     Testing:
         - /instructor/request-response/<request_id>
     Acceptance criteria:
-        -
+        - Response to a request has been submitted successfully
+        - Call DB
     '''
-    assert True, ""
+    response = requests.put(LOCALHOST_PATH + '/instructor/request-response/1', timeout = 5)
+    assert response.json() == {"message": "Case created successfully"}, \
+        "/instructor/request-response/ endpoint failed"
 
 def test_instructor_view_change_settings():
     '''
     Testing:
         - /instructor/settings/
     Acceptance criteria:
-        -
+        - Settings are changed successfully
+        - Call DB
     '''
-    assert True, ""
+    response = requests.put(LOCALHOST_PATH + '/instructor/settings/', timeout = 5)
+    assert response.json() == {"message": "Case created successfully"}, \
+        "/instructor/settings/ endpoint failed"
 
 # fake data to play with before DB connected
 usr3 = {
