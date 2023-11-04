5b7c4a7ddc023940696c3e85d44e85d6cc871a7c
Jun Youn <juuyoun01@gmail.com>
2023-11-04 18:47:37 +1100

    student home / view cases table adjusted headers

--- static/js/modules/requests.js
+++ static/js/modules/requests.js
@@ -344,62 +344,67 @@ function generateStudentCases(cases) {
 
         // Create table header row
 	    const headerRow = table.insertRow();
+
+        const CASE_TABLE_HEADERS = ["Request Type", "Course Name", "Assessment (if applicable)", "Current Status", "Date Updated"];
         
         // wait for the requests to load in and then continue
         sloadThreadsData(cases[i].case_id)
-            .then(threads => {
-                // gets the first request of the first thread just to fill out the headers
-                getLatestRequest(threads[0].thread_id)
-                        .then(request => {
-                            for (const key in request) {
-                                if (request.hasOwnProperty(key)) {
-                                    const th = document.createElement('th');
-                                    th.innerText = key;
-                                    headerRow.appendChild(th);
-                                }
-                            }
-                    
-                            // Add an empty header for the button column
-                            const emptyHeader = document.createElement('th');
-                            emptyHeader.textContent = '';
-                            headerRow.appendChild(emptyHeader);
-                        })
+
+                // Make the table headers
+
+                for (const key in CASE_TABLE_HEADERS) {
+                    const th = document.createElement('th');
+                    th.innerText = CASE_TABLE_HEADERS[key];
+                    headerRow.appendChild(th);
+                }
+
+                const emptyHeader = document.createElement('th');
+                emptyHeader.textContent = '';
+                headerRow.appendChild(emptyHeader);
 
                 // Create table data rows
                 threads.forEach(thread => {
-                    getLatestRequest(thread.thread_id)
-                        .then(request => {
 
-                            const row = table.insertRow();
-                            for (const key in request) {
-                                if (request.hasOwnProperty(key)) {
-                                    const cell = row.insertCell();
-                                    cell.className = 'tableEntry'; // Apply the CSS class to the cell
-                                cell.innerHTML = request[key];
-                                }
-                            }
+                    const row = table.insertRow();
+
+                    const requestTypeCell = row.insertCell();
+                    requestTypeCell.className = 'tableEntry';
+                    requestTypeCell.innerHTML = thread.request_type;
+
+                    const courseNameCell = row.insertCell();
+                    courseNameCell.className = 'tableEntry';
+                    courseNameCell.innerHTML = "COURSE NAME";
+
+                    const assessmentNameCell = row.insertCell();
+                    assessmentNameCell.className = 'tableEntry';
+                    assessmentNameCell.innerHTML = "ASSESSMENT";
+
+                    const currentStatusCell = row.insertCell();
+                    currentStatusCell.className = 'tableEntry';
+                    currentStatusCell.innerHTML = thread.current_status;
+
+                    const dateUpdatedCell = row.insertCell();
+                    dateUpdatedCell.className = 'tableEntry';
+                    dateUpdatedCell.innerHTML = thread.date_updated;
+
+                    const viewDetailsCell = row.insertCell();
+                    viewDetailsCell.className = 'tableEntry';
+                    const viewDetailsButton = document.createElement('button');
+                    viewDetailsButton.className = 'standardButton';
+                    viewDetailsButton.innerText = 'View Details';
+                    viewDetailsButton.onclick = function () {
+                        window.location.href = '/student/view-req/' + request.thread_id;
+                    }
+                    viewDetailsCell.appendChild(viewDetailsButton);
+                    expandableBoxSection.appendChild(table);
+            
+                    container.appendChild(expandableBox);
+                    container.appendChild(expandableBoxSection);
+                    container.appendChild(document.createElement("br"));
                 
-                            const viewDetailsCell = row.insertCell();
-                            viewDetailsCell.className = 'tableEntry';
-                            const viewDetailsButton = document.createElement('button');
-                            viewDetailsButton.className = 'standardButton';
-                            // Add the "View Details" button to the last cell for each request
-                            viewDetailsButton.innerText = 'View Details';
-                            viewDetailsButton.onclick = function () {
-                                window.location.href = '/student/view-req/' + request.thread_id;
-                            }
-                            viewDetailsCell.appendChild(viewDetailsButton);
-                        });
-                        // Append the table to the expandable box
-                        expandableBoxSection.appendChild(table);
-                
-                        container.appendChild(expandableBox);
-                        container.appendChild(expandableBoxSection);
-                        container.appendChild(document.createElement("br"));
-                })
-                    
-            })
-    };
+                });
+            });
+    }
 
     
 }
